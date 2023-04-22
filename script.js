455b0bb1cbd6cf934b61f5afd5b1c7dfaace031c
Dolma Bhuti <dbhuti95@gmail.com>
2023-04-22 17:41:53 -0400

    button and prompt added to change number of squares

--- script.js
+++ script.js
@@ -25,19 +25,34 @@ function createSquare(numOfSquares) {
     gridContainer.appendChild(squareDiv);
   }
 }
-//4. Add a button to the top of the screen that will send the user a popup asking for the number of squares per side for the new grid
+function clearSquare() {
+  while (gridContainer.hasChildNodes()) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+}
 createSquare(16);
 
+//5. add a button on top of screen that will pop up a prompt that asks for a number of squares per side for the new grid
+function btnClicked() {
+  let newNum = prompt("Please enter a number less than 101: ");
+  let num = parseInt(newNum);
+  if (newNum != null) {
+    if (newNum <= 100) {
+      //clear the old etch a sketch
+      clearSquare();
+
+      //create a new one
+      createSquare(newNum);
+    } else {
+      console.log("Number entered is greater than 100");
+    }
+  }
+}
 /*
-
-
-
-
 4. create a Hover effect on grid flex-items using a DOM eventlistener. (1 when you mouse enters a div and one where it leaves it)
 -- change color
   There are multiple ways to change the color of the divs, including:
   - adding a new class to the div.
   - changing the div’s background color using JavaScript.
 
-  5. add a button on top of screen that will pop up a prompt that asks for a number of squares per side for the new grid
 */
